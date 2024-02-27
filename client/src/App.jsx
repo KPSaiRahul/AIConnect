8b661c98a3ce9e1fca48ace1134d13c92526418d
KPSaiRahul <sairahulkp3@Gmail.com>
2024-02-27 12:17:14 +0530

    aitext and chat are working but not completion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "@/components/chat";
 function App() {
 
   return (
 
     <div className="app">
+      
       <BrowserRouter >
         <Routes>
           <Route path="/chat" element={<Chat />} />
